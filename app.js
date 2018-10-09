8c70a95c63f6064b7a93bd81431e27e10a2df0e2
Reginald Lamar Bramber II <architekespy@Reginalds-MacBook-Pro.local>
2018-10-09 13:43:47 -0400

    heroku

---
+++ app.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true})
+const connection = mongoose.connection
+connection.on('connected', () => {
+    console.log('a successful connection msg goes here')
+})
+connection.on('error', (err) => {
+    console.log('failed to connect msg goes here: ' + err)
+})
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(__dirname + '/client/build/'))
+
+//Routes 
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+})
+
+const usersController = require('./routes/usersController')
+
+app.use('/api/users/', usersController)
+
+
+
+
+module.exports = app;
